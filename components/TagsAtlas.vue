fee3a9911305d58148835e674f4e9c17bf82db16
Stefan Milic <stefan.milic@diwanee.com>
2018-06-01 14:59:55 +0200

    allTags Page and TagsAtlas comp with static content

---
+++ components/TagsAtlas.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+  <div class="c-tags-atlas">
+      <div class="c-tags-atlas__inner">
+          <h2 class='c-tags-atlas__title'>A</h2>
+          <ul class='c-tags-atlas__list'>
+              <li class='c-tags-atlas__index' v-for="(tag, index) in tags" :key='index+1'>
+                  <a class='c-tags-atlas__link' href="#" >{{tag}}</a>
+              </li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            tags:['Vivamus', 'vel', 'tellus', 'blandit', 'lacus', 'tincidunt', 'consectetur']
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import '~assets/scss/settings';
+
+.c-tags-atlas{
+
+    padding: 0 6%;
+    margin-bottom: 4.6rem;
+
+
+    &__title{
+        font-family: 'Airways';
+        @include fontSizeRem(38.67, 46);
+        color:$sun-yellow;
+    }
+
+    &__list{
+        margin-top: 1.7rem;
+    }
+    &__index{
+        margin-bottom: 0.65rem;
+    }
+    &__link{
+        @include fontSizeRem(16.40, 15);
+        transition: color .18s ease;
+
+        &:hover{
+            color:$sun-yellow;
+        }
+    }
+
+}
+</style>
---
+++ pages/alltags/index.vue
@@ -0,0 +1,57 @@
+<template lang="html">
+  <div class="p-all-tags main-wrapper">
+      <div class="inner-wrapper">
+          <div class="bread-crumbs">
+              home/alltags
+          </div>
+          <h1 class='page-title'>ALL TAGS</h1>
+          <div class="b-all-tags-atlas">
+              <tags-atlas v-for="item in items" :key='item'></tags-atlas>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import TagsAtlas from '~/components/TagsAtlas.vue'
+
+export default {
+    data(){
+        return{
+            items:[1,2,3,4,5],
+        }
+    },
+    created() {
+        this.$api.get(this.$api.queries.test_node)
+            .then(res => { console.log(res) });
+    },
+
+    components: {
+        TagsAtlas
+    }
+}
+</script>
+
+<style scoped lang='scss'>
+@import '~assets/scss/settings';
+.p-all-tags{
+    & .b-all-tags-atlas{
+        width: 100%;
+        margin: 0 auto;
+        margin-top: 4.8rem;
+        display: inline-flex;
+        flex-flow: row wrap;
+        justify-content: space-between;
+    }
+
+    .page-title{
+        @include fontSizeRem(38.67, 44.3);
+        font-family: 'Lato-Black';
+        color:$black;
+        font-weight: 900;
+        text-align: center;
+        margin-top: 4.8rem;
+    }
+}
+
+</style>
