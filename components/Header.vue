7f59fd3d131b7fa78ca803af590817e2e8d62e2f
Igor Bogdanovic <igor.bogdanovic@diwanee.com>
2018-06-01 14:39:26 +0200

    lato bold&black fonts added

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,91 @@
 <template>
   <nav class="b-nav">
-    
+    <div class="b-nav__hamburger u-only-mob">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
   </nav>
 </template>
 
-<style>
+<style lang="scss" scoped>
+@import '~assets/scss/settings';
+
+.b-nav {
+  width: 100%;
+  height: 4.4rem;
+  background-color: #000;
+
+  &__hamburger {
+    display: inline-block;
+    width: 3rem;
+    height: 2.5rem;
+    position: absolute;
+    top: 2.2rem;
+    right: 2rem;
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+    -webkit-transition: .3s ease-in-out;
+    -moz-transition: .3s ease-in-out;
+    -o-transition: .3s ease-in-out;
+    transition: .3s ease-in-out;
+    cursor: pointer;
+
+    span {
+      display: block;
+      position: absolute;
+      height: 0.3rem;
+      width: 100%;
+      background: $sun-yellow;
+      opacity: 1;
+      left: 0;
+      -webkit-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      transform: rotate(0deg);
+      -webkit-transition: .15s ease-in-out;
+      -moz-transition: .15s ease-in-out;
+      -o-transition: .15s ease-in-out;
+      transition: .15s ease-in-out;
+
+      &:nth-child(1) {
+        top: 0;
+      }
+      &:nth-child(2), &:nth-child(3) {
+        top: 1.1rem;
+      }
+      &:nth-child(4) {
+        top: 2.2rem;
+      }
+    }
+
+    &.is-active span {
+      &:nth-child(1) {
+        top: 1.1rem;
+        width: 0%;
+        left: 50%;
+      }
+      &:nth-child(2) {
+        -webkit-transform: rotate(45deg);
+        -moz-transform: rotate(45deg);
+        -o-transform: rotate(45deg);
+        transform: rotate(45deg);
+      }
+      &:nth-child(3) {
+        -webkit-transform: rotate(-45deg);
+        -moz-transform: rotate(-45deg);
+        -o-transform: rotate(-45deg);
+        transform: rotate(-45deg);
+      }
+      &:nth-child(4) {
+        top: 1.1rem;
+        width: 0%;
+        left: 50%;
+      }
+    }
+  }
+}
 </style>
---
+++ pages/igor/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="full-width-wrapper">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    AppHeader: Header
+  }
+}
+</script>
+
+<style>
+</style>
