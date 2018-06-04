3a2f4724c1e0757b367b667bffbc62d4ef0c6fc5
Wekios <diwanee@Bojans-MacBook-Pro.local>
2018-06-04 14:09:52 +0200

    adds cgu,t&c and error page.

---
+++ pages/wekios/404/index.vue
@@ -0,0 +1,89 @@
+<template>
+<div class="page">
+    <app-header/>
+    <!-- 404 Page -->
+
+    <div class="p-static p-404">
+
+        <div class="container">
+
+            <div class="c-404">
+                <p class="c-404__heading text--bolder">Take another <span>road</span></p>
+                <img src="~static/images/404-van.png" alt="van" class="c-404__van">
+            </div>
+
+            <button href="index.html" class="c-btn"><a href="http://localhost:3000/">back to homepage</a></button>
+
+        </div>
+
+    </div>
+        
+    <!-- 404 Page End-->
+    <app-footer/>
+</div>
+</template>
+
+<script>
+import AppHeader from "~/components/Header.vue";
+import AppFooter from "~/components/Footer.vue";
+
+export default {
+  created() {
+    this.$api.get(this.$api.queries.test_node).then(res => {
+      console.log(res);
+    });
+  },
+  components: {
+    AppHeader,
+    AppFooter
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/scss/settings";
+
+.p-404 {
+    margin-top: 13rem;
+    @include breakpoint(desktop) {
+        margin-top: 8rem;
+    }
+
+    .c-btn {
+        margin-bottom: 5rem;
+    }
+
+}
+
+.c-404 {
+
+    position: relative;
+    text-align: center;
+    margin-bottom: 7.6rem;
+    @include breakpoint(desktop) {
+    }
+
+    &__heading {
+    @include fontSizeRem(23.4,68);
+    letter-spacing: -0.025em;
+    color: $sun-yellow;
+    position: absolute;
+    z-index: -1;
+    top: -1.4rem;
+    left: 1.5rem;
+    @include breakpoint(desktop) {
+        top: -3.7rem;
+        left: 8rem;
+    }
+
+        span {
+            @include fontSizeRem(45.9,132.1);
+            vertical-align: text-top;
+        }
+    }
+
+
+}
+
+
+</style>
