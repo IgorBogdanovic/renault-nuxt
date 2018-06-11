589cc062b3cca2851c30a508aaaf25d8b9cabe72
Wekios <diwanee@Bojans-MacBook-Pro.local>
2018-06-11 11:59:42 +0200

    Reconfigure accordion

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -2,16 +2,33 @@
 
   <article class="accordion">
 
-    <div class="accordion__element" @click="open">
+    <div class="accordion__element" @click="show = !show">
 
-      <div class="accordion__heading">
+      <div class="accordion__element__heading">
         <p class="text--bolder">Lorem ipsum dolor sit amet?</p>
-        <img src="~/static/images/arrow.svg" alt="arrow" width="36px" height="36px">
+        <img src="~/static/images/arrow.svg" :class="arrowClasses" alt="arrow" width="36px" height="36px">
       </div>
 
-      <div class="accordion__text">
-        <p v-show="opened"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, placeat error assumenda perferendis voluptas iste dignissimos, dolores rem officiis at alias maiores. Assumenda quidem nemo ipsam consequuntur optio, perspiciatis debitis. </p>
+      <transition name="slide-fade" >
+        <div class="accordion__element__text" v-if="show">
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, placeat error assumenda perferendis voluptas iste dignissimos, dolores rem officiis at alias maiores. Assumenda quidem nemo ipsam consequuntur optio, perspiciatis debitis. </p>
+        </div>
+      </transition>
+
+    </div>
+    
+    <div class="accordion__element" @click="show = !show">
+
+      <div class="accordion__element__heading">
+        <p class="text--bolder">Lorem ipsum dolor sit amet?</p>
+        <img src="~/static/images/arrow.svg" :class="arrowClasses" alt="arrow" width="36px" height="36px">
       </div>
+
+      <transition name="slide-fade" >
+        <div class="accordion__element__text" v-if="show">
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, placeat error assumenda perferendis voluptas iste dignissimos, dolores rem officiis at alias maiores. Assumenda quidem nemo ipsam consequuntur optio, perspiciatis debitis. </p>
+        </div>
+      </transition>
 
     </div>
 
@@ -23,12 +40,15 @@
 export default {
   data() {
     return {
-      opened: false
+      show: true
     };
   },
-  methods: {
-    open() {
-      this.opened = !this.opened;
+  methods: {},
+  computed: {
+    arrowClasses() {
+      return {
+        "is-closed": !this.show
+      };
     }
   }
 };
@@ -46,18 +66,44 @@ export default {
   &__element {
     border-bottom: 0.1rem solid #efeeee;
     padding: 0 5%;
-  }
+    cursor: pointer;
 
-  &__heading {
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1.2rem 0;
+    &__heading {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      align-items: center;
+      padding: 1.2rem 0;
 
-    img {
-      border-radius: 50%;
-      background-color: $white;
+      img {
+        border-radius: 50%;
+        background-color: $white;
+        transition: 0.3s ease-in-out;
+      }
+
+      img.is-closed {
+        -ms-transform: rotate(180deg); /* IE 9 */
+        -webkit-transform: rotate(180deg); /* Safari 3-8 */
+        transform: rotate(180deg);
+      }
+    }
+
+    &__text {
+      p {
+        padding: 1rem;
+      }
+    }
+
+    .slide-fade-enter-active {
+      transition: all 0.2s ease;
+    }
+    .slide-fade-leave-active {
+      transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
+    }
+    .slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+      transform: translateY(-10px);
+      opacity: 0;
     }
   }
 }
