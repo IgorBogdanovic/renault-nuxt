042f124174908d12c9438b982d5d0d9158a222b4
Stefan Milic <stefan.milic@diwanee.com>
2018-06-12 10:42:31 +0200

    faq slider fx

--- pages/stefan/index.vue
+++ pages/stefan/index.vue
@@ -1,63 +1,20 @@
 <template lang="html">
   <div class="main-wrapper">
      <div class="inner-wrapper">
-         <p>{{articlesList}}</p>
+         <!-- <p>{{articlesList}}</p> -->
          <NewsOfTheWeek/>
          <HeroArticles/>
-        <!-- Slider main container -->
-        <div class="swiper-container">
-            <!-- Additional required wrapper -->
-            <div class="swiper-wrapper">
-                <!-- Slides -->
-                <div class="swiper-slide">
-                    <div class="swiper-slide__media">
-                        <img srcset="https://dummyimage.com/416x352/c9c9c9/000000.png" alt="…">
-                    </div>
-                    <div class="swiper-slide__content">
-                          <h2 class='swiper-slide__content__title'>My Van & Traffic Regulation</h2>
-                          <p class='swiper-slide__content__description'>
-                              The UK boasts several ski resorts offering seasonal business opportunities to entrepreneurs on wheels. Here are a few..
-                          </p>
-                      </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="swiper-slide__media">
-                        <img srcset="https://dummyimage.com/416x352/c9c9c9/000000.png" alt="…">
-                    </div>
-                    <div class="swiper-slide__content">
-                          <h2 class='swiper-slide__content__title'>My Van & Traffic Regulation</h2>
-                          <p class='swiper-slide__content__description'>
-                              The UK boasts several ski resorts offering seasonal business opportunities to entrepreneurs on wheels. Here are a few..
-                          </p>
-                      </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="swiper-slide__media">
-                        <img srcset="https://dummyimage.com/416x352/c9c9c9/000000.png" alt="…">
-                    </div>
-                    <div class="swiper-slide__content">
-                          <h2 class='swiper-slide__content__title'>My Van & Traffic Regulation</h2>
-                          <p class='swiper-slide__content__description'>
-                              The UK boasts several ski resorts offering seasonal business opportunities to entrepreneurs on wheels. Here are a few..
-                          </p>
-                      </div>
-                </div>
-            </div>
-
-            <div class="swiper-pagination"></div>
-
-            <!-- If we need navigation buttons -->
-            <div class="swiper-prev">⬅</div>
-            <div class="swiper-next">⬅</div>
-        </div>
+         <FaqMiniSlider/>
      </div>
   </div>
 </template>
 
 <script>
+import Swiper        from 'swiper'; //npm module
 import NewsOfTheWeek from '~/components/blocks/NewsOfTheWeek.vue'
 import HeroArticles  from '~/components/blocks/HeroArticles.vue'
-import Swiper        from 'swiper'; //npm module
+import FaqMiniSlider from '~/components/sliders/FaqMiniSlider.vue'
+
 
 export default {
 
@@ -79,6 +36,7 @@ export default {
   components:{
         NewsOfTheWeek,
         HeroArticles,
+        FaqMiniSlider,
   },
 
   mounted(){
@@ -119,88 +77,6 @@ export default {
 
 <style scoped lang='scss'>
 @import '~assets/scss/settings';
-@import '~assets/scss/plugins/swiper-min';
-    .swiper-container {
-        width: 79%;
-        margin-left:21%;
-    }
-
-    .swiper-wrapper{
-
-    }
-    .swiper-pagination{
-        position: absolute;
-        top:13.3rem;
-        right:0;
-        text-align: right;
-        z-index: 100;
-        padding-right: 6.1rem;
-        @include fontSizeRem(15,15)
-
-    }
-
-    .swiper-prev{
-        position: absolute;
-        top: 13.3rem;
-        right: 11.4rem;
-        z-index: 100;
-        @include fontSizeRem(15,15)
-    }
-    .swiper-next{
-        transform: scaleX(-1);
-        position: absolute;
-        top: 13.3rem;
-        right: 3.4rem;
-        z-index: 100;
-        @include fontSizeRem(15,15)
-    }
-
-    .swiper-slide{
-        display: flex;
-        box-sizing: border-box;
-
-        @include breakpoint(desktop){
-            flex-flow: row nowrap;
-        }
-
-        &__media{
-
-            @include breakpoint(desktop){
-                width:48%;
-            }
-        }
-
-        &__content{
-            padding-top: 4.2rem;
-            padding: 12.9rem 3.4% 0;
-
-            @include breakpoint(desktop){
-                width:52%;
-                padding: 12.9rem 3.9% 0;
-            }
-
-            &__title{
-                font-family: 'Lato-Bold';
-                line-height: 1.22;
-                @include fontSizeRem(23,23)
-                width:45%;
-                @include breakpoint(desktop){
-                    width:45%;
-                }
-            }
-            &__description{
-                @include fontSizeRem(14,14)
-                width:81%;
-                line-height: 1.55;
-                padding-top: 1.3rem;
-                color:$warm-grey;
-                @include breakpoint(desktop){
-                    width:81%;
-                    padding-top: 1.3rem;
-                }
-            }
-        }
-    }
 
     .b-articles-list-hero{
         @include breakpoint(desktop){
