4dfa2ab7fe8783376d9e11f420bad457bb548275
Stefan Milic <stefan.milic@diwanee.com>
2018-06-22 16:53:34 +0200

    news of the week

--- components/blocks/NewsOfTheWeek.vue
+++ components/blocks/NewsOfTheWeek.vue
@@ -1,13 +1,31 @@
 <template lang="html">
-    <section class="b-news-otw">
-        <div class="b-news-otw__inner">
+    <section class="b-news-otw main-wrapper">
+        <div class="b-news-otw__inner inner-wrapper">
             <h2 class='b-news-otw__title'><span class='bolder'>NEWS</span> <span class='cursive'>of the</span> <span class='regular'>WEEK</span></h2>
             <div class="b-news-otw__col-left">
-                <article-preview v-for="(preview, index) in previews.slice(0,2)" :key="index" :prev="preview"></article-preview>
+                <app-article-preview
+                    :prev="previews[0]"
+                    :thumbor="{ imgDesk : {width: 200, height:264}, imgMob: {width: 156, height: 192}}"
+                    :tag="tag"
+                ></app-article-preview>
+                <app-article-preview
+                    :prev="previews[1]"
+                    :thumbor="{ imgDesk : {width: 344, height:456}, imgMob: {width: 160, height: 275}}"
+                    :tag="tag"
+                ></app-article-preview>
             </div>
             <div class="b-news-otw__col-right">
-                <article-preview  :prev="previews[2]"></article-preview>
-                <article-preview  :prev="previews[3]"  class='component--landscape'></article-preview>
+                <app-article-preview
+                    :prev="previews[2]"
+                    :thumbor="{ imgDesk : {width: 492, height:400}, imgMob: {width: 336, height: 284}}"
+                    :tag="tag"
+                ></app-article-preview>
+                <app-article-preview
+                     class='component--landscape'
+                    :prev="previews[3]"
+                    :thumbor="{ imgDesk : {width: 272, height:224}, imgMob: {width: 200, height: 164}}"
+                    :tag="tag"
+                ></app-article-preview>
             </div>
         </div>
         <div class="b-news-otw__cta">
@@ -17,53 +35,16 @@
 </template>
 
 <script>
-import ArticlePreview from '~/components/articles/ArticlePreview.vue'
 
 export default {
+    props: ['previews'],
     data() {
         return {
-            previews: [
-                {
-                    // image: 'https://dummyimage.com/156x192/c9c9c9/000000.png',//mobile
-                    image: 'https://dummyimage.com/200x264/c9c9c9/000000.png',
-                    imageAlt: 'some alt',
-                    tagged: true,
-                    tag: 'MY BUSINESS',
-                    title: 'From Start-up to Growth: What to Expect When Business Picks Up',
-                    desc: 'Starting your own small business can be all-consuming in terms of time and attention, leaving you unprepared for a…'
-                },
-                {
-                    // image: 'https://dummyimage.com/160x275/c9c9c9/000000.png',//mobile
-                    image: 'https://dummyimage.com/344x456/c9c9c9/000000.png',
-                    imageAlt: 'some alt',
-                    tagged: true,
-                    tag: 'MY BUSINESS',
-                    title: 'How to Set Up a Mobile Showroom',
-                    desc: 'A mobile showroom for your business really isn’t so different from the tried and tested mobile retail outlet...'
-                },
-                {
-                    // image: 'https://dummyimage.com/336x284/c9c9c9/000000.png',//mobile
-                    image: 'https://dummyimage.com/492x400/c9c9c9/000000.png',
-                    imageAlt: 'some alt',
-                    tagged: true,
-                    tag: 'other tag',
-                    title: '5 Novel Ideas for A Food Truck',
-                    desc: 'UK foodies have officially embraced street-food culture, but that doesn’t make it easier to know where to start…'
-                },
-                {
-                    // image: 'https://dummyimage.com/200x164/c9c9c9/000000.png',//mobile
-                    image: 'https://dummyimage.com/272x224/c9c9c9/000000.png',
-                    imageAlt: 'some alt',
-                    tagged: true,
-                    tag: 'fan',
-                    title: '5 Things to Know to Drive Around London',
-                    desc: 'London traffic congestion is well-known, as is the growing array of measures aiming to reduce it. Here are 5…'
-                }
-            ]
+            tag: {
+                visible: true,
+                text: 'News of The Week'
+            }
         }
-    },
-    components: {
-        ArticlePreview
     }
 }
 </script>
@@ -160,6 +141,7 @@ export default {
 
     &__cta{
         padding-top: 4.4rem;
+        text-align: center;
         @include breakpoint(desktop){
             padding-top: 8rem;
         }
